de051ef93279553f6c45b568d143dd477818d630
Anh Quoc Tran <quoc.ta@softworld.vn>
2021-09-24 10:08:10 +0700

    add root export

---
+++ index.ts
@@ -0,0 +1,25 @@
+export * from './auth';
+export { 
+  fromRef as RtdbFromRef,
+  ListenEvent,
+  ListenerMethods,
+  Query,
+  QueryChange,
+  auditTrail,
+  changeToData,
+  list,
+  listVal,
+  object,
+  objectVal,
+  stateChanges
+} from './database';
+
+export * from './firestore';
+
+export * from './functions';
+
+export * from './performance';
+
+export * from './remote-config';
+
+export * from './storage';
